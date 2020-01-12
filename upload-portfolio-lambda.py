62f03595a7021882d8e426f462cd3abdf28868ea
David Clark <david.clark12@gmail.com>
2020-01-12 13:34:03 -0500

    fixing bug in index and updating lambda script.

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -9,20 +9,40 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:905657604453:deployPortfolioTopic')
 
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    location = {
+        "bucketName": 'portfoliobuild.davidmclark.info',
+        "objectKey": 'portfoliobuild.zip'
+    }
 
-    portfolio_bucket = s3.Bucket('portfolio.davidmclark.info')
-    build_bucket = s3.Bucket('portfoliobuild.davidmclark.info')
+    try:
+        job = event.get('CodePipeline.job')
 
-    portfolio_zip = io.BytesIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "BuildArtifact":
+                    location = artifact["location"]["s3Location"]
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        print("Building portfolio from " + str(location))
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    print("Portfolio Build Complete...")
-    topic.publish(Subject="Portfolio", Message="Portfolio build complete.")
+        portfolio_bucket = s3.Bucket('portfolio.davidmclark.info')
+        build_bucket = s3.Bucket(location["bucketName"])
+
+        portfolio_zip = io.BytesIO()
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print("Portfolio Build Complete...")
+        topic.publish(Subject="Portfolio", Message="Portfolio build complete.")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+    except:
+        topic.publish(Subject="Portfolio", Message="Portfolio build failed.")
+        raise
